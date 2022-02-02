9939c36207c6b1ab684bcd8b011cff2a927d25af
Akash <yadavakash1397@gmail.com>
2022-02-02 23:01:36 +0530

    get req added in akash package

--- src/main/java/AkashPackage/demo1_GET_Req.java
+++ src/main/java/AkashPackage/demo1_GET_Req.java
@@ -1,6 +1,40 @@
 package AkashPackage;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
 public class demo1_GET_Req
 {
+    @Test
+    public void getWeatherDetails()
+    {
+        String statusLine="HTTP/1.1 200 OK";
+        int statusCode=200;
+
+        //URl set
+        RestAssured.baseURI="https://restapi.demoqa.com/utilities/weather/city";
+
+        //create req object with URL
+       RequestSpecification req=RestAssured.given();
+
+       //send GET request and save response
+        Response res=req.request(Method.GET,"/Hyderabad");
+
+        //validate response status code
+        System.out.println("Response code:"+res.getStatusCode());
+        Assert.assertEquals(statusCode,res.getStatusCode());
+
+        //validate response status line
+        System.out.println("status line:"+res.getStatusLine());
+        Assert.assertEquals(statusLine,res.getStatusLine());
+
+        //print response body
+        System.out.println("Response body:"+res.getBody().asString());
+    }
 
 }

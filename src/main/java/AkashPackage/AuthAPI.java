ee818f54696e695363076c150b979f3f90a487f6
Akash <yadavakash1397@gmail.com>
2022-02-05 00:35:23 +0530

    Authenticaion added and response validated

---
+++ src/main/java/AkashPackage/AuthAPI.java
@@ -0,0 +1,33 @@
+package AkashPackage;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthAPI
+{
+    @Test
+    void authenticationAPI()
+    {
+        RestAssured.baseURI= "https://gorest.co.in/public/v2/";
+        RequestSpecification reqobj=RestAssured.given();
+        reqobj.header("Content-Type","application/json");
+        reqobj.header("Authorization","Bearer efb442b668c11d57b234abdcc3b7047a4f3b7db5fcd9961fcbc7dc2c55aa8b03");
+
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("name", "ak");
+        jsonObject.put("gender", "male");
+        jsonObject.put("email", "ak13@gmail.com");
+        jsonObject.put("status", "active");
+        reqobj.body(jsonObject.toJSONString());
+
+        Response response=reqobj.request(Method.POST,"users");
+        System.out.println(response.getBody().toString());
+        Assert.assertEquals(response.getStatusCode(),201);
+
+    }
+}
